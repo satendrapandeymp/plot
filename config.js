354ec825e6be55c263c2c947a17df0a9cb0df519
pandey <pandey@pandey.pandey.com>
2017-02-10 03:52:06 +0530

    plot of last 10 data

---
+++ config.js
@@ -0,0 +1,19 @@
+// host = '172.16.153.122';	// hostname or IP address
+host = 'kritsnam.in';	// hostname or IP address
+// host = '172.16.153.110';	// hostname or IP address
+port = 8080;
+topic = 'node/+/payload';		// topic to subscribe to
+useTLS = false;
+username = "web";
+password = "sockets";
+// username = "jjolie";
+// password = "aa";
+
+// path as in "scheme:[//[user:password@]host[:port]][/]path[?query][#fragment]"
+//    defaults to "/mqtt"
+//    may include query and fragment
+//
+// path = "/mqtt";
+// path = "/data/cloud?device=12345";
+
+cleansession = true;
